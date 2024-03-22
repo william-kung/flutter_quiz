e6e117cc4f7745dfd27af207b3dcc8d6376a4f1e
admin <william0129@gmail.com>
2024-03-22 14:05:50 +0800

    use ternary expression to switch screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -13,17 +13,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
   
   void switchScreen() {
     setState((){
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -38,7 +32,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomCenter,
               colors: [color1, color2],)
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen' 
+          ? StartScreen(switchScreen)
+          : const QuestionsScreen(),
           )
       ),
     );
