f119ef3eeeadd3a3421c02a18152071a93656523
admin <william0129@gmail.com>
2024-03-22 12:26:58 +0800

    multi-screen with iniState method

---
+++ lib/quiz.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/main.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz ({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+  
+  void switchScreen() {
+    setState((){
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [color1, color2],)
+          ),
+          child: activeScreen,
+          )
+      ),
+    );
+  }
+}
+
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-
-
+  const StartScreen(this.startQuiz, {super.key});
+  
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -12,10 +11,20 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            width: 300,
-            'assets/images/quiz-logo.png'
-          ),
+            Image.asset(
+              color: const Color.fromARGB(125, 255, 255, 255),
+              // More cpu efficient to put an overlay of color and set transparency compared to Opacity widget.
+              width: 300,
+              'assets/images/quiz-logo.png'
+            ),
+
+          // Opacity(
+          //   opacity: 0.1,
+          //   child: Image.asset(
+          //     width: 300,
+          //     'assets/images/quiz-logo.png'
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             style: TextStyle(
@@ -25,7 +34,14 @@ class StartScreen extends StatelessWidget {
             'Learn Flutter the fun way!'
             ),
           const SizedBox(height: 30),
-          OutlinedButton(onPressed: () {}, child: const Text('Start Quiz'))
+          OutlinedButton.icon(
+            onPressed: startQuiz, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz')
+          ),
         ],
       ),
     );
